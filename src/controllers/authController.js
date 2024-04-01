0afe54cdb613f4433a53a72a4cc018a6aea3339e
himeki204 <sonhz123@gmail.com>
2024-04-01 16:30:29 +0700

    update-validation

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -7,11 +7,18 @@ const authController = {
     registerUser: async (req, res) => {
         try {
           let errors = [];
+          const regex = /^[a-zA-Z0-9]+$/;
           const { username, email, password, fullname, repassword } = req.body;
       
           // Check required fields
           if (!username || !email || !fullname || !password || !repassword) {
             errors.push({ msg: 'Please fill all the fields' });
+          }
+          if (!regex.test(username)) {
+            errors.push({ msg: 'Username must contain only letters and numbers' });
+        } 
+          if (username.length > 20) {
+            errors.push({ msg: 'Username must be at maximum of 20 characters' });
           }
           if (password != repassword) {
             errors.push({ msg: 'Passwords do not match' });
@@ -19,8 +26,12 @@ const authController = {
           if (password.length < 6) {
             errors.push({ msg: 'Password must be at least 6 characters' });
           }
-          
-
+          if (!/^[a-zA-Z\s]*$/.test(fullname)) {
+            errors.push({ msg: 'Fullname contains characters other than letters and whitespace'})
+          }
+          if (/^\s*$/.test(username)){
+            errors.push({ msg: 'Missing credentials'})
+          }
           if (errors.length > 0) {
             return res.render('auth/register', { errors, username, email, password, fullname });
           }
@@ -36,7 +47,7 @@ const authController = {
           const newUser = new User({ username, email, password: hash, fullname });
           await newUser.save();
       
-          req.flash('success_msg', 'You are now registered and can log in');
+          req.flash('success_msg', 'You are now registered and can login');
           res.redirect('/auth/login');
         } catch (err) {
           console.log(err);
